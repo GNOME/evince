e0bd0a5e28d6ac5fda6835ccfe1cb6eb4963a794
Michael Meeks <mmeeks@src.gnome.org>
1999-10-06 17:59:38 +0000

    missing file: duh.

---
+++ pdf/xpdf/bonobo-application-x-pdf.h
@@ -0,0 +1,17 @@
+#ifndef BONOBOAPPLICATIONXPDF_H
+#define BONOBOAPPLICATIONXPDF_H
+
+/*
+ *   This is only neccessary since the gpdf container is
+ * very tightly coupled to the component, for ergonomic reasons.
+ */
+
+#define VERB_FIRST "FirstPage"
+#define VERB_PREV  "PrevPage"
+#define VERB_NEXT  "NextPage"
+#define VERB_LAST  "LastPage"
+#define VERB_Z_FIT "ZoomFit"
+#define VERB_Z_IN  "ZoomIn"
+#define VERB_Z_OUT "ZoomOut"
+
+#endif /* BONOBOAPPLICATIONXPDF_H */
