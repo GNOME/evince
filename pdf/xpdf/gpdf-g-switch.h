c99b7a5bded1e43ec1177124ce38961c0b0b9b69
Martin Kretzschmar <mkretzschmar@src.gnome.org>
2002-10-12 11:46:34 +0000

    add GNOME_PDF.server, GNOME_PDF.server.in
    * xpdf/.cvsignore: add GNOME_PDF.server, GNOME_PDF.server.in
    
    * xpdf/pdf-view.cc: don't #include bonobo_application_x_pdf.h,
    update for new GObject, GSignal, Bonobo signatures
    (setup_pixmap): don't use double buffering
    
    * xpdf/gtkgesture.h: s/BEGIN_GNOME_DECLS/G_BEGIN_DECLS/
    
    * xpdf/gpdf-persist-stream.h, xpdf/gpdf-control.h, pdf-view.h
    (#includes): update, use gpdf-g-switch.h,
    s/BEGIN_GNOME_DECLS/G_BEGIN_DECLS/
    
    * xpdf/gpdf-persist-stream.cc (#includes): update
    (load_pdf_from_stream): Bonobo_Unknown_ref our stream
    (delete_pdf_doc_full): unref our stream
    (all around) update for new GObject, GSignal, Bonobo signatures
    (gpdf_persist_stream_new, gpdf_persist_stream_construct): take iid
    parameter to be passed on to bonobo_persist_stream_construct
    
    * xpdf/gpdf-embeddable-view.cc: don't include
    bonobo-application-x-pdf.h
    
    * xpdf/gpdf-control.cc: PARENT_TYPE is BONOBO_TYPE_CONTROL;
    update for new GObject, GtkWidget, Bonobo signatures
    
    * xpdf/gpdf-control-ui.xml: use gtk-stock-pixmaps
    
    * xpdf/bonobo-application-x-pdf.cc (#includes): use
    gpdf-g-switch.h, update
    (gpdf_factory): don't create embeddable, gpdf_persist_stream_new
    takes iid parameter
    (init_gpdf_factory, init_corba, init_bonobo): kill
    (main): no need to init gdk_rgb; use BONOBO_FACTORY_INIT,
    bonobo_generic_factory_main
    
    * xpdf/Makefile.am (INCLUDES, LDADD): update
    (bin_PROGRAMS): move gnome-pdf-viewer to libexec_PROGRAMS
    (gnome_pdf_viewer_SOURCES): nobody needs
    bonobo-application-x-pdf.h, don't build embeddable stuff
    (oafdir, oaf_DATA): kill
    (server_in_files, serverdir): add, do libexec sed job on
    .server.in.in file, add @INTLTOOL_SERVER_RULE@
    (uidir): s/gnome/gnome-2.0/
    
    * xpdf/GOutputDev.cc (#includes): use gpdf-g-switch.h
    (GOutputDev::GOutputDev): get size and depth of pixmap with
    gdk_drawable_get_foo background color to white
    (all around): gdk_region_foo now destructively update their first
    argument, use gdk_rgb to find colors
    (drawImageMask): don't gdk_image_new_bitmap
    
    * xpdf/gpdf-g-switch.h: new file, include it when switching
    between goo and glib headers
    
    * xpdf/GNOME_PDF.server.in.in: factory is in @LIBEXECDIR@
    s|GNOME/GenericFactory|Bonobo/GenericFactory|, make it
    translatable, kill embeddable stanza
    
    * xpdf/GNOME_PDF.oafinfo: remove
    
    * xpdf/BonoboStream.cc:
    s/Bonobo_Stream_SEEK_SET/Bonobo_Stream_SeekSet/ et al. (see
    libbonobo/ChangeLog#2002-03-31)
    
    * shell/gpdf.c (#includes): use G2 headers
    (main): copy from eog
    (struct _Component): kill (was embeddable code)
    (USE_CONTROL): kill #define, kill all code surrounded by #if
    !USE_CONTROL (i.e. embeddable code)
    (all around): add Corba_Environment argument where it's needed now
    (gw_control_load_pdf): s/BonoboStream \*stream/Bonobo_Stream
    stream/.  get the stream using monikers, unref it later
    (gw_drag_data_received): use uri handling code from gnome-vfs
    (verb_HelpAbout_cb): update for new gnome_about_new
    (verb_DebugDumpXml_cb): kill
    (gw_destroy): may be called more than once, so update assertions
    (gw_finalize): is a GObject method now, chain up to parent
    (class_init): update for GObject methods
    (gw_setup_window): blend of bonobo_window_construct and old
    gw_setup_geometry
    (gw_setup_geometry): kill
    (gw_setup_toplevel_ui): use BonoboUIContainer from BonoboWindow
    (gpdf_window_construct): use gw_setup_window, not
    bonobo_window_construct, not gw_setup_geometry
    
    * shell/gpdf-window.h: s/BEGIN_GNOME_DECLS/G_BEGIN_DECLS/, kill
    struct _Component decl (was used for embeddable)
    
    * shell/gpdf-window-ui.xml: kill DebugDumpXml verb
    
    * shell/Makefile.am: update INCLUDES, LD_ADD;
    (gpdf_SOURCES): add missing gpdf-window.h
    (uidir): s/gnome/gnome-2.0/
    
    * configure.in: clean it a bit; set version to 0.100 (version was
    at 0.90.1 for ever and I don't want to call it 1.01.1, this is not
    1.something quality code); use GNOME2 macros;
    (i18n): update according to G2 porting guide
    (gnome-lib-checks): use PKG_CHECK
    
    * autogen.sh: PKG_NAME="gpdf:", USE_GNOME2_MACROS
    
    * .cvsignore: add intltool-... files

---
+++ pdf/xpdf/gpdf-g-switch.h
@@ -0,0 +1,22 @@
+/* 
+ * When including goo and glib headers in one file:
+ *
+ * #include "GList.h"
+ * #include "gpdf-g-switch.h"
+ * #include "glib.h"
+ * < more glib/gtk+/gnome headers >
+ * #include "gpdf-g-switch.h"
+ */
+
+
+#ifdef GPDF_GOO
+#  undef GString
+#  undef GList
+#  undef GDir
+#  undef GPDF_GOO
+#else
+#  define GString G_String
+#  define GList   G_List
+#  define GDir    G_Dir
+#  define GPDF_GOO
+#endif
