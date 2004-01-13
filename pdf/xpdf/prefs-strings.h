d8a7126bc5fa1f5e3d89184972d3b9c8694233f1
Remi Cohen-Scali <rcoscali@cvs.gnome.org>
2004-01-13 01:25:00 +0000

    Defines for gconf keys
    2004-01-13  Remi Cohen-Scali  <rcoscali@cvs.gnome.org>
    
    	* xpdf/prefs-strings.h: Defines for gconf keys

---
+++ pdf/xpdf/prefs-strings.h
@@ -0,0 +1,4 @@
+/* Window appeareance */
+#define CONF_WINDOWS_SIDEBAR_PAGE "/apps/gpdf/UI/Windows/sidebar_page"
+#define CONF_WINDOWS_SIDEBAR_SIZE "/apps/gpdf/UI/Windows/sidebar_size"
+#define CONF_WINDOWS_SHOW_SIDEBAR "/apps/gpdf/UI/Windows/show_sidebar"
