b2e7a9f7c44d07a2af27c8b7a52c9fc4e19f615c
Marco Pesenti Gritti <mpg@redhat.com>
2005-07-11 10:25:56 +0000

    Fix for dbus 0.33. bug #309975
    2005-07-11  Marco Pesenti Gritti  <mpg@redhat.com>
    
            * shell/main.c: (load_files_remote):
    
            Fix for dbus 0.33. bug #309975

--- shell/main.c
+++ shell/main.c
@@ -92,7 +92,15 @@ load_files_remote (const char **files)
                                                    "/org/gnome/evince/Evince",
                                                    "org.gnome.evince.Application");
 	if (!files) {
-#if DBUS_VERSION < 35
+#if DBUS_VERSION <= 33
+		call = dbus_g_proxy_begin_call (remote_object, "OpenWindow", DBUS_TYPE_INVALID);
+
+		if (!dbus_g_proxy_end_call (remote_object, call, &error, DBUS_TYPE_INVALID)) {
+			g_warning (error->message);
+			g_clear_error (&error);
+			return FALSE;
+		}
+#elif DBUS_VERSION == 34
 		call = dbus_g_proxy_begin_call (remote_object, "OpenWindow", G_TYPE_INVALID);
 
 		if (!dbus_g_proxy_end_call (remote_object, call, &error, G_TYPE_INVALID)) {
@@ -117,7 +125,18 @@ load_files_remote (const char **files)
 
 		uri = gnome_vfs_make_uri_from_shell_arg (files[i]);
 		page_label = ev_page_label ? ev_page_label : ""; 
-#if DBUS_VERSION < 35
+#if DBUS_VERSION <= 33
+		call = dbus_g_proxy_begin_call (remote_object, "OpenURI",
+						DBUS_TYPE_STRING, &uri,
+						DBUS_TYPE_STRING, &page_label,
+						DBUS_TYPE_INVALID);
+
+		if (!dbus_g_proxy_end_call (remote_object, call, &error, DBUS_TYPE_INVALID)) {
+			g_warning (error->message);
+			g_clear_error (&error);
+			result = FALSE;
+		}
+#elif DBUS_VERSION == 34
 		call = dbus_g_proxy_begin_call (remote_object, "OpenURI",
 						G_TYPE_STRING, uri,
 						G_TYPE_STRING, page_label,
