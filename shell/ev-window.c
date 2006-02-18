f7f4a48b0cb3c4268e5eb1d0b639e28dab8fc3f0
Nickolay V. Shmyrev <nshmyrev@src.gnome.org>
2006-02-18 09:49:37 +0000

    Ask filechooser to confirm overwrite. Fix for the bug #328266. Thanks to
    	* configure.ac:
    	* shell/ev-window.c: (ev_window_cmd_save_as):
    
    	Ask filechooser to confirm overwrite. Fix for the bug #328266.
    	Thanks to Gary Coady <gary@lyranthe.org>

--- shell/ev-window.c
+++ shell/ev-window.c
@@ -1251,7 +1251,10 @@ ev_window_cmd_save_as (GtkAction *action, EvWindow *ev_window)
 
 	ev_document_types_add_filters (fc, ev_window->priv->document);
 	gtk_dialog_set_default_response (GTK_DIALOG (fc), GTK_RESPONSE_OK);
-	
+
+#ifdef HAVE_GTK_FILE_CHOOSER_SET_DO_OVERWRITE_CONFIRMATION
+	gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER (fc), TRUE);	
+#endif	
 	file_name = gnome_vfs_format_uri_for_display (ev_window->priv->uri);
 	base_name = g_path_get_basename (file_name);
 	gtk_file_chooser_set_current_name (GTK_FILE_CHOOSER (fc), base_name);
