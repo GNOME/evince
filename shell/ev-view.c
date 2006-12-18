67f80b85d70a6d577e36c9578b854003a7d910ca
Carlos Garcia Campos <carlosgc@gnome.org>
2006-12-18 13:55:05 +0000

    Acquire lock document when drawing "Loading..." text so that only one
    2006-12-18  Carlos Garcia Campos  <carlosgc@gnome.org>
    	* shell/ev-view.c: (draw_loading_text):
    	Acquire lock document when drawing "Loading..." text so that only one
    	thread will use FontConfig at the same time. Fixes bug #374750.

--- shell/ev-view.c
+++ shell/ev-view.c
@@ -2390,6 +2390,8 @@ draw_loading_text (EvView       *view,
 
 	const char *loading_text = _("Loading...");	
 
+	ev_document_doc_mutex_lock ();
+	
 	layout = gtk_widget_create_pango_layout (GTK_WIDGET (view), loading_text);
 
 	font_desc = pango_font_description_new ();
@@ -2420,6 +2422,8 @@ draw_loading_text (EvView       *view,
 
 	pango_font_description_free (font_desc);
 	g_object_unref (layout);
+
+	ev_document_doc_mutex_unlock ();
 }
 
 static void
