76dc886f8488897f3ebf1506ab671f29739607ff
Jose Aliste <jose.aliste@gmail.com>
2009-12-20 16:12:45 +0100

    Add synctex parser to cut-n-paste directory.

---
+++ cut-n-paste/synctex/update-synctex-from-TL.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# update-synctex-from-TL.sh
+#
+# Get latest synctex parser from TexLive SVN repository.
+SVN_URI=svn://tug.org/texlive/trunk/Build/source/texk/web2c/synctexdir
+SCRIPT_NAME=update-synctex-from-TL.sh
+FILES="synctex_parser_version.txt synctex_parser.c  synctex_parser.h  synctex_parser_local.h  synctex_parser_utils.c  synctex_parser_utils.h"
+
+  echo "Obtaining latest version of the sources"
+  for FILE in $FILES 
+  do
+    svn export $SVN_URI/$FILE
+  done
+
