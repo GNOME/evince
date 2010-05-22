af534ddb4d8992c0d58a52966f2fc40a7dde9067
Carlos Garcia Campos <carlosgc@gnome.org>
2010-05-22 15:39:47 +0200

    Use always the session bus instead of starter
    I'm not sure it makes sense using the system bus

--- shell/ev-daemon.c
+++ shell/ev-daemon.c
@@ -377,7 +377,7 @@ main (gint argc, gchar **argv)
 
 	g_type_init ();
 
-        connection = g_bus_get_sync (G_BUS_TYPE_STARTER, NULL, &error);
+        connection = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, &error);
         if (connection == NULL) {
                 g_printerr ("Failed to get bus connection: %s\n", error->message);
                 g_error_free (error);
