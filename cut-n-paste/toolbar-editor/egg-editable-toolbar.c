c16ad49219beb256b7f6cea1d1ce8d8a4a2b654c
Jose Aliste <jose.aliste@gmail.com>
2010-06-06 19:50:31 -0400

    [toolbar-editor] Syncing to GDK API changes

--- cut-n-paste/toolbar-editor/egg-editable-toolbar.c
+++ cut-n-paste/toolbar-editor/egg-editable-toolbar.c
@@ -695,12 +695,12 @@ toolbar_drag_data_received_cb (GtkToolbar         *toolbar,
           gint tpos = get_toolbar_position (etoolbar, GTK_WIDGET (toolbar));
           egg_toolbars_model_add_item (etoolbar->priv->model, tpos, ipos, name);
           gtk_drag_finish (context, TRUE,
-			   gdk_drag_context_get_action (context) == GDK_ACTION_MOVE, time);
+			   gdk_drag_context_get_selected_action (context) == GDK_ACTION_MOVE, time);
         }
       else
         {
           gtk_drag_finish (context, FALSE,
-			   gdk_drag_context_get_action (context) == GDK_ACTION_MOVE, time);
+			   gdk_drag_context_get_selected_action (context) == GDK_ACTION_MOVE, time);
         }
     }
 
