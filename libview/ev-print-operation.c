151819917d5a85b0a4d5f0d5910c6a834caa9189
José Aliste <jaliste@src.gnome.org>
2012-12-06 09:56:56 -0300

    libview: Correct error message when postscript not supported
    see https://bugzilla.gnome.org/show_bug.cgi?id=683528

--- libview/ev-print-operation.c
+++ libview/ev-print-operation.c
@@ -1160,7 +1160,7 @@ ev_print_operation_export_print_dialog_response_cb (GtkDialog              *dial
 		g_set_error_literal (&export->error,
                                      GTK_PRINT_ERROR,
                                      GTK_PRINT_ERROR_GENERAL,
-                                     _("Printing is not supported on this printer."));
+                                     _("Postscript is not supported by this printer."));
 		g_signal_emit (op, signals[DONE], 0, GTK_PRINT_OPERATION_RESULT_ERROR);
 		
 		return;
