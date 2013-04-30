GNOME/evince

commit 151819917d5a85b0a4d5f0d5910c6a834caa9189

libview: Correct error message when postscript not supported
see https://bugzilla.gnome.org/show_bug.cgi?id=683528

libview/ev-print-operation.c
@@ -1160,7 +1160,7 @@ ev_print_operation_export_print_dialog_response_cb (GtkDialog              *dial
 		g_set_error_literal (&export->error,
                                      GTK_PRINT_ERROR,
                                      GTK_PRINT_ERROR_GENERAL,
-                                     _("Printing is not supported on this printer."));
+                                     _("Postscript is not supported by this printer."));
 		g_signal_emit (op, signals[DONE], 0, GTK_PRINT_OPERATION_RESULT_ERROR);
 		
 		return;

commit b1ceb9e2007168412e9167e309136e98d4af86b8

libview: Fix typo in PostScript printer error message
Replace Postscript with PostScript in an error message which was
introduced in commit 151819917d5a85b0a4d5f0d5910c6a834caa9189 to
fix #683528.

Bug #695597.

libview/ev-print-operation.c
@@ -1172,7 +1172,7 @@ ev_print_operation_export_print_dialog_response_cb (GtkDialog              *dial
 		g_set_error_literal (&export->error,
                                      GTK_PRINT_ERROR,
                                      GTK_PRINT_ERROR_GENERAL,
-                                     _("Postscript is not supported by this printer."));
+                                     _("PostScript is not supported by this printer."));
 		g_signal_emit (op, signals[DONE], 0, GTK_PRINT_OPERATION_RESULT_ERROR);
 		
 		return;