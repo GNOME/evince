bed036acf9bccac9a13a4b019381f0b69169b9d2
Carlos Garcia Campos <carlosgc@gnome.org>
2014-03-17 13:38:28 +0100

    ev-view-presentation: Propagate unhandled key events
    This was preventing shortcuts like CTRL+w from working.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=604834

--- libview/ev-view-presentation.c
+++ libview/ev-view-presentation.c
@@ -1114,6 +1114,9 @@ ev_view_presentation_key_press_event (GtkWidget   *widget,
 	if (pview->state == EV_PRESENTATION_END)
                 return gtk_bindings_activate_event (G_OBJECT (widget), event);
 
+        if (event->state != 0)
+                return gtk_bindings_activate_event (G_OBJECT (widget), event);
+
 	switch (event->keyval) {
 	case GDK_KEY_b:
 	case GDK_KEY_B:
