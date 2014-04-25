110a98a7f0c1b528f0c35c382dce0d2c73660256
Joanmarie Diggs <jdiggs@igalia.com>
2014-04-25 01:30:30 -0400

    libview: Update ATK_STATE_SHOWING when the visible page range changes
    https://bugzilla.gnome.org/show_bug.cgi?id=728664

--- libview/ev-view.c
+++ libview/ev-view.c
@@ -785,6 +785,9 @@ view_update_range_and_current_page (EvView *view)
 					view->start_page,
 					view->end_page,
 					view->selection_info.selections);
+	ev_view_accessible_set_page_range (EV_VIEW_ACCESSIBLE (view->accessible),
+					   view->start_page,
+					   view->end_page);
 
 	if (ev_pixbuf_cache_get_surface (view->pixbuf_cache, view->current_page))
 	    gtk_widget_queue_draw (GTK_WIDGET (view));
