bf7a4fcdf6211e11d94dfe9fed82113d92e24eb7
Ting-Wei Lan <lantw@src.gnome.org>
2016-09-06 13:32:12 +0800

    browser-plugin: Replace gdkwayland.h include with gdk.h
    GDK_WINDOWING_WAYLAND macro is defined in gdkconfig.h, not gdkwayland.h,
    so including gdk.h is sufficient. This also fixes build on systems not
    supporting Wayland.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=770924

--- browser-plugin/EvBrowserPluginMain.cpp
+++ browser-plugin/EvBrowserPluginMain.cpp
@@ -22,7 +22,7 @@
 #include "EvMemoryUtils.h"
 #include "npfunctions.h"
 #include "npruntime.h"
-#include <gdk/gdkwayland.h>
+#include <gdk/gdk.h>
 
 static NPNetscapeFuncs *browser;
 static unique_gptr<char> mimeDescription;
