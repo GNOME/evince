ccbeb9cca23b84ddb2d4f1e1d7e745a92d219ddb
Bastien Nocera <hadess@hadess.net>
2017-08-30 21:54:04 +0200

    unarr: Update LZMA SDK
    Using lzma1701.7z from:
    https://sourceforge.net/projects/sevenzip/files/LZMA%20SDK/
    
    https://bugzilla.gnome.org/show_bug.cgi?id=787035

--- cut-n-paste/unarr/lzmasdk/Precomp.h
+++ cut-n-paste/unarr/lzmasdk/Precomp.h
@@ -5,11 +5,6 @@
 #define __7Z_PRECOMP_H
 
 /* #include "Compiler.h" */
-#ifdef _MSC_VER
-#pragma warning(disable : 4456) // declaration of * hides previous local declaration
-#pragma warning(disable : 4457) // declaration of * hides function parameter
-#pragma warning(disable : 4996) // This function or variable may be unsafe
-#endif
 /* #include "7zTypes.h" */
 
 #endif
