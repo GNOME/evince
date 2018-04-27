7344bb1e0268ff8f4d33391d53e95b1ea268adb1
Nelson Benítez León <nbenitezl+gnome@gmail.com>
2018-04-27 23:27:13 +0500

    sidebar-annotations: Add context menu for sidebar annotations
    Implement new contextual menu for annotations in sidebar, we
    reuse annotation actions used by EvWindow in its context
    menu, so same actions will be shown in both places.
    
    We also implement the GtkWidget "popup" signal so the context
    menu can also be triggered by keyboard (usually Shift-F10 or
    specific context menu key in some keyboards).
    
    https://bugzilla.gnome.org/show_bug.cgi?id=795631

--- shell/ev-window.h
+++ shell/ev-window.h
@@ -94,6 +94,8 @@ EvHistory      *ev_window_get_history                    (EvWindow       *ev_win
 EvDocumentModel *ev_window_get_document_model            (EvWindow       *ev_window);
 void            ev_window_focus_view                     (EvWindow       *ev_window);
 GtkWidget      *ev_window_get_toolbar			 (EvWindow	 *ev_window);
+void            ev_window_handle_annot_popup             (EvWindow       *ev_window,
+                                                          EvAnnotation   *annot);
 
 G_END_DECLS
 
