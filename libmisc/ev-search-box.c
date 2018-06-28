ae8c1c263fe703526e72dcd2f7a71be58de8bd22
Germán Poo-Caamaño <gpoo@gnome.org>
2018-06-28 19:00:21 -0400

    libmisc: Let the buttons in search bar get focus
    The buttons in the search bar allow the user to navigate through
    the previous and next match. For user who rely on keyboard navigation
    is important they can select those buttons (other than shortcuts).
    
    The new search entry bar was introduced in commit 16314cd5. There is
    no rationale to make the unfocusable. It might have trying to mimic
    the behaviour of the old search entry, which also had the issue.
    
    Closes #216

--- libmisc/ev-search-box.c
+++ libmisc/ev-search-box.c
@@ -588,14 +588,12 @@ ev_search_box_init (EvSearchBox *box)
 
         priv->prev_button = gtk_button_new_from_icon_name ("go-up-symbolic", GTK_ICON_SIZE_MENU);
         gtk_widget_set_tooltip_text (priv->prev_button, _("Find previous occurrence of the search string"));
-        gtk_widget_set_can_focus (priv->prev_button, FALSE);
         gtk_widget_set_sensitive (priv->prev_button, FALSE);
         gtk_container_add (GTK_CONTAINER (box), priv->prev_button);
         gtk_widget_show (priv->prev_button);
 
         priv->next_button = gtk_button_new_from_icon_name ("go-down-symbolic", GTK_ICON_SIZE_MENU);
         gtk_widget_set_tooltip_text (priv->next_button, _("Find next occurrence of the search string"));
-        gtk_widget_set_can_focus (priv->next_button, FALSE);
         gtk_widget_set_sensitive (priv->next_button, FALSE);
         gtk_container_add (GTK_CONTAINER (box), priv->next_button);
         gtk_widget_show (priv->next_button);
