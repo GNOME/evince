a8a2dac1c3df94c0d5001d4039d0751221d448f4
Germán Poo-Caamaño <gpoo@gnome.org>
2018-07-02 13:01:04 -0400

    libmisc: Enable popover menu in EvPageActionWidget
    Enable to create an EvPageActionWidget with a menu, to provide
    a contextual menu (Popover) for actions related to navigate the
    document (or whatever the user would like to add).

--- libmisc/ev-page-action-widget.h
+++ libmisc/ev-page-action-widget.h
@@ -1,6 +1,7 @@
 /*
  *  Copyright (C) 2003, 2004 Marco Pesenti Gritti
  *  Copyright (C) 2003, 2004 Christian Persch
+ *  Copyright (C) 2018       Germán Poo-Caamaño
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -27,8 +28,9 @@
 
 G_BEGIN_DECLS
 
-#define EV_TYPE_PAGE_ACTION_WIDGET (ev_page_action_widget_get_type ())
-#define EV_PAGE_ACTION_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), EV_TYPE_PAGE_ACTION_WIDGET, EvPageActionWidget))
+#define EV_TYPE_PAGE_ACTION_WIDGET    (ev_page_action_widget_get_type ())
+#define EV_PAGE_ACTION_WIDGET(obj)    (G_TYPE_CHECK_INSTANCE_CAST ((obj), EV_TYPE_PAGE_ACTION_WIDGET, EvPageActionWidget))
+#define EV_IS_PAGE_ACTION_WIDGET(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EV_TYPE_PAGE_ACTION_WIDGET))
 
 typedef struct _EvPageActionWidget EvPageActionWidget;
 typedef struct _EvPageActionWidgetClass EvPageActionWidgetClass;
@@ -41,14 +43,17 @@ struct _EvPageActionWidgetClass
 			        EvLink             *link);
 };
 
-GType ev_page_action_widget_get_type          (void) G_GNUC_CONST;
+GType      ev_page_action_widget_get_type           (void) G_GNUC_CONST;
 
-void ev_page_action_widget_update_links_model (EvPageActionWidget *proxy,
-					       GtkTreeModel       *model);
+void       ev_page_action_widget_update_links_model (EvPageActionWidget *proxy,
+                                                     GtkTreeModel       *model);
 
-void ev_page_action_widget_set_model          (EvPageActionWidget *action_widget,
-					       EvDocumentModel    *doc_model);
-void ev_page_action_widget_grab_focus         (EvPageActionWidget *proxy);
+void       ev_page_action_widget_set_model          (EvPageActionWidget *action_widget,
+                                                     EvDocumentModel    *doc_model);
+void       ev_page_action_widget_grab_focus         (EvPageActionWidget *proxy);
+
+GtkWidget *ev_page_action_widget_new                (GMenu              *menu);
+gboolean   ev_page_action_widget_get_popup_shown    (EvPageActionWidget *action_widget);
 
 G_END_DECLS
 
