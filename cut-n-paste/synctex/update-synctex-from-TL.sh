c3e7b60aac62cda82780ffbca14857402bd2ca7d
Germán Poo-Caamaño <gpoo@gnome.org>
2018-08-02 15:56:17 -0400

    synctex: Update script to sync against synctex repository

--- cut-n-paste/synctex/update-synctex-from-TL.sh
+++ cut-n-paste/synctex/update-synctex-from-TL.sh
@@ -4,7 +4,7 @@
 # Get latest synctex parser from TexLive SVN repository.
 SVN_URI=svn://tug.org/texlive/trunk/Build/source/texk/web2c/synctexdir
 SCRIPT_NAME=update-synctex-from-TL.sh
-FILES="synctex_parser_version.txt synctex_parser.c  synctex_parser.h  synctex_parser_local.h  synctex_parser_utils.c  synctex_parser_utils.h"
+FILES="synctex_parser_version.txt synctex_parser.c  synctex_parser.h  synctex_parser_advanced.h synctex_parser_local.h  synctex_parser_utils.c  synctex_parser_utils.h synctex_version.h"
 
   echo "Obtaining latest version of the sources"
   for FILE in $FILES 
