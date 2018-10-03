d189395373aad656c32b64ecdfd6eb3a26d10e53
Germán Poo-Caamaño <gpoo@gnome.org>
2018-10-03 14:56:56 -0300

    Set back D-Bus application name to lowercase
    * Set application id lowercase
    * Make the daemon and application names match
    * Make the icons and desktop files match the
      application id
    * Keep the D-BUS interfaces backwards compatible,
      and enable
      the flatpak version to launch the daemon.
    * Take care of translations after renaming files.
    
    This commit also changes the application id for flatpak,
    which is a compromise.
    
    Backported from master, commit f99c554309.

--- build-aux/flatpak/local-build.sh
+++ build-aux/flatpak/local-build.sh
@@ -4,11 +4,11 @@ scriptdir=`dirname $0`
 olddir=`pwd`
 cd $scriptdir
 
-rm -f org.gnome.Evince.flatpak
+rm -f org.gnome.evince.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
 flatpak-builder --ccache --force-clean _build org.gnome.Evince.json --repo=_repo
-flatpak build-bundle _repo org.gnome.Evince.flatpak org.gnome.Evince master
+flatpak build-bundle _repo org.gnome.evince.flatpak org.gnome.evince master
 
 cd $olddir
