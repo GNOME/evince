642e6a7b6942c8a2317dc219eadbc0e812876b9e
Quipyowert2 <Quipyowert2@users.noreply.github.com>
2018-10-30 01:05:52 -0700

    End the va_list before returning from _synctex_merge_strings.
    This adds a va_end() call to _synctex_merge_strings in two places
    where it would return without ending the va_list first.

--- cut-n-paste/synctex/synctex_parser_utils.c
+++ cut-n-paste/synctex/synctex_parser_utils.c
@@ -354,6 +354,7 @@ char * _synctex_merge_strings(const char * first,...) {
 		size_t len = strlen(temp);
 		if(UINT_MAX-len<size) {
 			_synctex_error("!  _synctex_merge_strings: Capacity exceeded.");
+			va_end(arg);
 			return NULL;
 		}
 		size+=len;
@@ -374,6 +375,7 @@ char * _synctex_merge_strings(const char * first,...) {
 						_synctex_error("!  _synctex_merge_strings: Copy problem");
 						free(result);
 						result = NULL;
+						va_end(arg);
 						return NULL;
 					}
 					dest += size;
