674ab1de72145485f8ea2a715a6ff67a5c99548b
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2019-01-06 14:49:13 +0100

    unbreak build when compiled without dbus
    APPLICATION_NAME is also used in ev_application_new() since 9363553a
    
    Fixes #1055

--- shell/ev-application.c
+++ shell/ev-application.c
@@ -64,8 +64,8 @@ struct _EvApplicationClass {
 
 G_DEFINE_TYPE (EvApplication, ev_application, GTK_TYPE_APPLICATION)
 
-#ifdef ENABLE_DBUS
 #define APPLICATION_NAME             "org.gnome.Evince"
+#ifdef ENABLE_DBUS
 #define APPLICATION_DBUS_OBJECT_PATH "/org/gnome/evince/Evince"
 #define APPLICATION_DBUS_INTERFACE   "org.gnome.evince.Application"
 
