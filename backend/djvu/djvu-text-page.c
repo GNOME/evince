a2abe0922040e8e7e4f4f6ab0184633b5927b733
Jason Crain <jcrain@src.gnome.org>
2019-03-30 21:48:37 -0600

    djvu: use find_text_offset interface for search
    This allows the sidebar to show search results for DjVu documents.
    
    The current method of showing results in the sidebar relies on doing a
    mapping from the search result's area on the page to the document's
    text. This does not work for DjVu documents because they often do not
    specify the exact location of characters beyond the location of a line
    or word. With the find_text_offset interface, the backend returns an
    EvDocumentFindMatch struct, which contains both the area and the offset
    into the document's text, so this mapping is not needed.
    
    EvDocumentFindMatch also allows an improvement for multi-line search
    results. The current interface only returns a single rectangle for each
    result, so for a multi-line search, the rectangle is expanded to cover a
    larger area, the bounding box of whole search result.
    EvDocumentFindMatch has a list of rectangles, so we can return several
    rectangles instead.

--- backend/djvu/djvu-text-page.c
+++ backend/djvu/djvu-text-page.c
@@ -22,6 +22,7 @@
 #include <glib.h>
 #include <libdjvu/miniexp.h>
 #include "djvu-text-page.h"
+#include "ev-document-find.h"
 
 
 /**
@@ -107,15 +108,13 @@ djvu_text_page_selection_process_text (DjvuTextPage *page,
 	if (page->text || p == page->start) {
 		char *token_text = (char *) miniexp_to_str (miniexp_nth (5, p));
 		if (page->text) {
-			char *new_text =
-			    g_strjoin (delimit & 2 ? "\n" : 
-			    	       delimit & 1 ? " " : NULL,
-				       page->text, token_text,
-				       NULL);
-			g_free (page->text);
-			page->text = new_text;
+			if (delimit & 2)
+				g_string_append_c (page->text, '\n');
+			else if (delimit & 1)
+				g_string_append_c (page->text, ' ');
+			g_string_append (page->text, token_text);
 		} else
-			page->text = g_strdup (token_text);
+			page->text = g_string_new (token_text);
 		if (p == page->end) 
 			return FALSE;
 	}
@@ -241,7 +240,7 @@ char *
 djvu_text_page_copy (DjvuTextPage *page, 
 		     EvRectangle  *rectangle)
 {
-	char* text;
+	char* text = NULL;
 	
 	page->start = miniexp_nil;
 	page->end = miniexp_nil;
@@ -250,8 +249,10 @@ djvu_text_page_copy (DjvuTextPage *page,
 	                          page->text_structure, 0);
 	
 	/* Do not free the string */	  
-	text = page->text;
-	page->text = NULL;
+	if (page->text) {
+		text = g_string_free (page->text, FALSE);
+		page->text = NULL;
+	}
 	
 	return text;
 }
@@ -300,8 +301,11 @@ djvu_text_page_position (DjvuTextPage *page,
  * @p: s-expression to append
  * @start: first s-expression in the selection
  * @end: last s-expression in the selection
- * 
- * Appends the rectangle defined by @p to the internal bounding box rectangle.
+ * @linebreak: if TRUE, a new rectangle should be added to the list instead of
+ *     being unioned to the current rectangle.
+ *
+ * Appends the rectangle defined by @p to the internal `lines` list of
+ * rectangles.
  * 
  * Returns: whether the end was not reached in this s-expression
  */
@@ -309,20 +313,23 @@ static gboolean
 djvu_text_page_sexpr_process (DjvuTextPage *page, 
                               miniexp_t     p,
                               miniexp_t     start, 
-                              miniexp_t     end)
+                              miniexp_t     end,
+                              gboolean      linebreak)
 {
-	if (page->bounding_box || p == start) {
+	if (page->lines || p == start) {
 		EvRectangle *new_rectangle = ev_rectangle_new ();
 		new_rectangle->x1 = miniexp_to_int (miniexp_nth (1, p));
 		new_rectangle->y1 = miniexp_to_int (miniexp_nth (2, p));
 		new_rectangle->x2 = miniexp_to_int (miniexp_nth (3, p));
 		new_rectangle->y2 = miniexp_to_int (miniexp_nth (4, p));
-		if (page->bounding_box) {
-			djvu_text_page_union (page->bounding_box,
+		if (page->lines && !linebreak) {
+			djvu_text_page_union ((EvRectangle *) page->lines->data,
 					      new_rectangle);
 			g_free (new_rectangle);
-		} else
-			page->bounding_box = new_rectangle;
+		} else {
+			page->lines = g_list_prepend (page->lines,
+						      new_rectangle);
+		}
 		if (p == end)
 			return FALSE;
 	}
@@ -335,7 +342,9 @@ djvu_text_page_sexpr_process (DjvuTextPage *page,
  * @p: tree to append
  * @start: first s-expression in the selection
  * @end: last s-expression in the selection
- * 
+ * @linebreak: if TRUE, the next s-expression should add a new rectangle
+ *     instead of unioning with the current rectangle.
+ *
  * Walks the tree in @p and extends the rectangle with 
  * djvu_text_page_process() for all s-expressions between @start and @end.
  * 
@@ -345,46 +354,58 @@ static gboolean
 djvu_text_page_sexpr (DjvuTextPage *page, 
 		      miniexp_t p,
 		      miniexp_t start, 
-		      miniexp_t end)
+		      miniexp_t end,
+		      gboolean linebreak)
 {
         miniexp_t deeper;
 
 	g_return_val_if_fail (miniexp_consp (p) && miniexp_symbolp
 			      (miniexp_car (p)), FALSE);
 
+        /* 'char' and 'word' expressions should be merged into a single
+         * rectangle. others such as 'line' and 'para' expressions should be
+         * split into separate rectangles. */
+	if (miniexp_car (p) != page->char_symbol &&
+	    miniexp_car (p) != page->word_symbol)
+		linebreak = TRUE;
+
 	deeper = miniexp_cddr (miniexp_cdddr (p));
 	while (deeper != miniexp_nil) {
 		miniexp_t str = miniexp_car (deeper);
 		if (miniexp_stringp (str)) {
 			if (!djvu_text_page_sexpr_process
-			    (page, p, start, end))
+			    (page, p, start, end, linebreak))
 				return FALSE;
 		} else {
 			if (!djvu_text_page_sexpr
-			    (page, str, start, end))
+			    (page, str, start, end, linebreak))
 				return FALSE;
 		}
+		linebreak = FALSE;
 		deeper = miniexp_cdr (deeper);
 	}
 	return TRUE;
 }
 
 /**
- * djvu_text_page_box:
+ * djvu_text_page_lines:
  * @page: #DjvuTextPage instance
  * @start: first s-expression in the selection
  * @end: last s-expression in the selection
- * 
- * Builds a rectangle that contains all s-expressions in the given range.
+ *
+ * Builds a list of rectangles that contains all s-expressions in the given
+ * range. Expressions forming a single line, 'word' and 'char' expressions,
+ * will be unioned into a single rectangle.  Others will be separate rectangles
+ * in the list.
  */
-static EvRectangle *
-djvu_text_page_box (DjvuTextPage *page,
-		    miniexp_t     start, 
-		    miniexp_t     end)
+static GList *
+djvu_text_page_lines (DjvuTextPage *page,
+		      miniexp_t     start,
+		      miniexp_t     end)
 {
-	page->bounding_box = NULL;
-	djvu_text_page_sexpr (page, page->text_structure, start, end);
-	return page->bounding_box;
+	page->lines = NULL;
+	djvu_text_page_sexpr (page, page->text_structure, start, end, FALSE);
+	return g_list_reverse (page->lines);
 }
 
 /**
@@ -402,7 +423,7 @@ djvu_text_page_append_text (DjvuTextPage *page,
 			    gboolean      case_sensitive, 
 			    gboolean      delimit)
 {
-	char *token_text;
+	const char *token_text;
 	miniexp_t deeper;
 	
 	g_return_if_fail (miniexp_consp (p) && 
@@ -416,25 +437,55 @@ djvu_text_page_append_text (DjvuTextPage *page,
 		if (miniexp_stringp (data)) {
 			DjvuTextLink link;
 			link.position = page->text == NULL ? 0 :
-			    strlen (page->text);
+			    page->text->len;
 			link.pair = p;
 			g_array_append_val (page->links, link);
 
-			token_text = (char *) miniexp_to_str (data);
-			if (!case_sensitive)
-				token_text = g_utf8_casefold (token_text, -1);
-			if (page->text == NULL)
-				page->text = g_strdup (token_text);
-			else {
-				char *new_text =
-				    g_strjoin (delimit ? " " : NULL,
-					       page->text, token_text,
-					       NULL);
-				g_free (page->text);
-				page->text = new_text;
+			if (page->text == NULL) {
+				page->text = g_string_new (NULL);
+				if (page->utf8_map)
+					g_array_set_size (page->utf8_map, 0);
+				else
+					page->utf8_map = g_array_new (FALSE,
+								      FALSE,
+								      sizeof (gint));
+				page->utf8_count = 0;
+			} else if (delimit) {
+				g_string_append_c (page->text, ' ');
+				g_array_append_val (page->utf8_map, page->utf8_count);
+				page->utf8_count++;
 			}
-			if (!case_sensitive)
-				g_free (token_text);
+
+			/* Append token to page text and build map of bytes to
+			 * utf8 offsets. The map is needed because casefold can
+			 * convert a character to multiple characters and we'll
+			 * need to convert back to offsets in original text. */
+			token_text = miniexp_to_str (data);
+			while (*token_text != '\0') {
+				char *casefold_text;
+				const gchar *next;
+				int i;
+
+				next = g_utf8_next_char (token_text);
+				if (!case_sensitive) {
+					casefold_text = g_utf8_casefold (token_text,
+						                         next - token_text);
+					g_string_append (page->text, casefold_text);
+					for (i = 0; i < strlen (casefold_text); i++)
+						g_array_append_val (page->utf8_map,
+							            page->utf8_count);
+					g_free (casefold_text);
+				} else {
+					g_string_append_len (page->text,
+						             token_text,
+							     next - token_text);
+					for (i = 0; i < next - token_text; i++)
+						g_array_append_val (page->utf8_map,
+							            page->utf8_count);
+				}
+				page->utf8_count++;
+				token_text = next;
+			}
 		} else
 			djvu_text_page_append_text (page, data, 
 						    case_sensitive, delimit);
@@ -451,28 +502,56 @@ djvu_text_page_append_text (DjvuTextPage *page,
  * Searches the page for the given text. The results list has to be 
  * externally freed afterwards.
  */
-void 
+GPtrArray *
 djvu_text_page_search (DjvuTextPage *page, 
-		       const char   *text)
+		       const char   *text,
+		       gdouble       height,
+		       gdouble       dpi)
 {
-	char *haystack = page->text;
+	char *haystack = page->text->str;
 	int search_len;
-	EvRectangle *result;
+	GList *area, *l;
+	EvDocumentFindMatch *match;
+	GPtrArray *results;
+	gsize start_offset;
+	gsize end_offset;
+	gdouble tmp;
+
 	if (page->links->len == 0)
-		return;
+		return NULL;
+
+        results = g_ptr_array_new_with_free_func ((GDestroyNotify) ev_document_find_match_free);
 
 	search_len = strlen (text);
 	while ((haystack = strstr (haystack, text)) != NULL) {
-		int start_p = haystack - page->text;
+		int start_p = haystack - page->text->str;
 		miniexp_t start = djvu_text_page_position (page, start_p);
 		int end_p = start_p + search_len - 1;
 		miniexp_t end = djvu_text_page_position (page, end_p);
-		result = djvu_text_page_box (page, start, end);
-		g_assert (result);
-		page->results = g_list_prepend (page->results, result);
+
+		area = djvu_text_page_lines (page, start, end);
+		g_assert (area);
+
+                /* Fix up coordinates to start from top instead of bottom and
+                 * convert to 72 dpi. */
+                for (l = area; l != NULL; l = l->next) {
+			EvRectangle *rect = (EvRectangle *) l->data;
+
+			rect->x1 *= 72.0 / dpi;
+			rect->x2 *= 72.0 / dpi;
+			tmp = rect->y1;
+			rect->y1 = height - rect->y2 * 72.0 / dpi;
+			rect->y2 = height - tmp * 72.0 / dpi;
+                }
+
+		start_offset = g_array_index (page->utf8_map, gint, start_p),
+		end_offset = g_array_index (page->utf8_map, gint, end_p) + 1;
+		match = ev_document_find_match_new (area, start_offset,
+		                                    end_offset);
+		g_ptr_array_add (results, match);
 		haystack = haystack + search_len;
 	}
-	page->results = g_list_reverse (page->results);
+	return results;
 }
 
 
@@ -521,7 +600,10 @@ djvu_text_page_new (miniexp_t text)
 void 
 djvu_text_page_free (DjvuTextPage *page)
 {
-	g_free (page->text);
+	if (page->text)
+		g_string_free (page->text, TRUE);
+	if (page->utf8_map)
+		g_array_free (page->utf8_map, TRUE);
 	g_array_free (page->links, TRUE);
 	g_free (page);
 }
