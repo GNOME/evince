37555bdac1a3abf37b482a66e1a2aced1db100f8
Liel Fridman <lielft@gmail.com>
2019-07-18 19:29:29 +0300

    Added an open with menu.
    This is done to solve issue 840, and heavily influenced by the implementation in EOG.

--- shell/ev-toolbar.h
+++ shell/ev-toolbar.h
@@ -58,6 +58,8 @@ GtkWidget    *ev_toolbar_get_page_selector  (EvToolbar *ev_toolbar);
 void          ev_toolbar_set_mode           (EvToolbar     *ev_toolbar,
 					     EvToolbarMode  mode);
 EvToolbarMode ev_toolbar_get_mode           (EvToolbar     *ev_toolbar);
+void          ev_toolbar_open_with          (EvToolbar     *ev_toolbar,
+                         const char *file, GVariant *parameter);
 
 G_END_DECLS
 
