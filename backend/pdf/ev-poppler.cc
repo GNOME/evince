GNOME/evince

commit e2ad8611a4c58f09d78def78500a2c10a21ac48b

pdf: support 'de facto' tooltip feature
Most pdf readers implement a tooltip feature by
showing the string content of 'TU' field of a
widget annotation that is not linked to any
form field.

Normally, widget annotations carry a reference to a
form field which are used together to implement the
different form widgets. But, the PDF spec does not
forbid standalone (i.e. not linked to any form field)
widget annotations, and the fact is they're been used
by most pdf readers to show a tooltip when the area
of that AnnotWidget is hovered.

According to issue reported files, 'pdfTeX' seems to
be one of the pdf producers incorporating this tooltip
feature.

The support for this feature has been recently been
added to poppler-glib with following api:

gchar*
poppler_form_field_get_alternate_ui_name (PopplerFormField *field)

so this commit uses that api to show tooltips on pdf's
that carry them. For that we add a new 'alt_ui_name'
field to EvFormField and show it as a tooltip when
the field's area is hovered.

Evince issue #842

Poppler issue:
https://gitlab.freedesktop.org/poppler/poppler/issues/34

backend/pdf/ev-poppler.cc
@@ -2665,11 +2665,15 @@ ev_form_field_from_poppler_field (PdfDocument      *pdf_document,
 	gdouble      font_size;
 	gboolean     is_read_only;
 	PopplerAction *action;
+	gchar       *alt_ui_name = NULL;
 
 	id = poppler_form_field_get_id (poppler_field);
 	font_size = poppler_form_field_get_font_size (poppler_field);
 	is_read_only = poppler_form_field_is_read_only (poppler_field);
 	action = poppler_form_field_get_action (poppler_field);
+#if POPPLER_CHECK_VERSION(0, 88, 0)
+	alt_ui_name = poppler_form_field_get_alternate_ui_name (poppler_field);
+#endif
 
 	switch (poppler_form_field_get_field_type (poppler_field)) {
 	        case POPPLER_FORM_FIELD_TEXT: {
@@ -2759,6 +2763,7 @@ ev_form_field_from_poppler_field (PdfDocument      *pdf_document,
 
 	ev_field->font_size = font_size;
 	ev_field->is_read_only = is_read_only;
+	ev_field->alt_ui_name = alt_ui_name;
 
 	if (action)
 		ev_field->activation_link = ev_link_from_action (pdf_document, action);

libdocument/ev-form-field.c
@@ -45,6 +45,7 @@ ev_form_field_init (EvFormField *field)
 	field->page = NULL;
 	field->changed = FALSE;
 	field->is_read_only = FALSE;
+	field->alt_ui_name = NULL;
 }
 
 static void
@@ -56,6 +57,7 @@ ev_form_field_finalize (GObject *object)
 	field->page = NULL;
 
 	g_clear_object (&field->activation_link);
+	g_clear_pointer (&field->alt_ui_name, g_free);
 
 	(* G_OBJECT_CLASS (ev_form_field_parent_class)->finalize) (object);
 }

libdocument/ev-form-field.h
@@ -113,6 +113,7 @@ struct _EvFormField
 
 	EvPage  *page;
 	gboolean changed;
+	gchar   *alt_ui_name;
 };
 
 struct _EvFormFieldClass

libview/ev-view.c
@@ -2205,7 +2205,7 @@ ev_view_handle_cursor_over_xy (EvView *view, gint x, gint y)
 			ev_view_set_cursor (view, EV_VIEW_CURSOR_NORMAL);
 	}
 
-	if (link || annot)
+	if (link || annot || (field && field->alt_ui_name))
 		g_object_set (view, "has-tooltip", TRUE, NULL);
 }
 
@@ -4911,6 +4911,26 @@ get_annot_area (EvView       *view,
 				       annot, area);
 }
 
+static void
+get_field_area (EvView       *view,
+	        gint          x,
+	        gint          y,
+	        EvFormField  *field,
+	        GdkRectangle *area)
+{
+	EvMappingList *field_mapping;
+	gint           page;
+	gint           x_offset = 0, y_offset = 0;
+
+	x += view->scroll_x;
+	y += view->scroll_y;
+
+	find_page_at_location (view, x, y, &page, &x_offset, &y_offset);
+
+	field_mapping = ev_page_cache_get_form_field_mapping (view->page_cache, page);
+	ev_view_get_area_from_mapping (view, page, field_mapping, field, area);
+}
+
 static gboolean
 ev_view_query_tooltip (GtkWidget  *widget,
 		       gint        x,
@@ -4919,6 +4939,7 @@ ev_view_query_tooltip (GtkWidget  *widget,
 		       GtkTooltip *tooltip)
 {
 	EvView       *view = EV_VIEW (widget);
+	EvFormField  *field;
 	EvLink       *link;
 	EvAnnotation *annot;
 	gchar        *text;
@@ -4937,6 +4958,17 @@ ev_view_query_tooltip (GtkWidget  *widget,
 
 			return TRUE;
 		}
+	}
+
+	field = ev_view_get_form_field_at_location (view, x, y);
+	if (field && field->alt_ui_name && *(field->alt_ui_name) != '\0') {
+		GdkRectangle field_area;
+
+		get_field_area (view, x, y, field, &field_area);
+		gtk_tooltip_set_text (tooltip, field->alt_ui_name);
+		gtk_tooltip_set_tip_area (tooltip, &field_area);
+
+		return TRUE;
 	}
 
 	link = ev_view_get_link_at_location (view, x, y);

commit b1de8c28445492377706a85e0cab9b61fb948fdf

pdf: Reimplement 'de facto' tooltip with no ABI break
Structs in EvForm are not opaque, and
Commit e2ad8611a4c introduced a ABI change

backend/pdf/ev-poppler.cc
@@ -52,6 +52,7 @@
 #include "ev-document-annotations.h"
 #include "ev-document-attachments.h"
 #include "ev-document-text.h"
+#include "ev-form-field-private.h"
 #include "ev-selection.h"
 #include "ev-transition-effect.h"
 #include "ev-attachment.h"
@@ -2780,7 +2781,7 @@ ev_form_field_from_poppler_field (PdfDocument      *pdf_document,
 
 	ev_field->font_size = font_size;
 	ev_field->is_read_only = is_read_only;
-	ev_field->alt_ui_name = alt_ui_name;
+	ev_form_field_set_alternate_name (ev_field, alt_ui_name);
 
 	if (action)
 		ev_field->activation_link = ev_link_from_action (pdf_document, action);

libdocument/ev-form-field-private.h
@@ -1,0 +1,42 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; c-indent-level: 8 -*- */
+/* this file is part of evince, a gnome document viewer
+ *
+ *  Copyright (C) 2020 Germán Poo-Caamaño <gpoo@gnome.org>
+ *
+ * Evince is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Evince is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#if !defined (EVINCE_COMPILATION)
+#error "Only <evince-document.h> can be included directly."
+#endif
+
+#ifndef EV_FORM_FIELD_PRIVATE_H
+#define EV_FORM_FIELD_PRIVATE_H
+
+//#include <glib-object.h>
+
+#include "ev-form-field.h"
+
+G_BEGIN_DECLS
+
+/* EvFormField base class */
+gchar *ev_form_field_get_alternate_name (EvFormField *field);
+void   ev_form_field_set_alternate_name (EvFormField *field,
+					 gchar       *alternative_text);
+
+G_END_DECLS
+
+#endif /* !EV_FORM_FIELD_PRIVATE_H */
+

libdocument/ev-form-field.c
@@ -1,6 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8; c-indent-level: 8 -*- */
 /* this file is part of evince, a gnome document viewer
  *
+ *  Copyright (C) 2020 Germán Poo-Caamaño <gpoo@gnome.org>
  *  Copyright (C) 2007 Carlos Garcia Campos <carlosgc@gnome.org>
  *  Copyright (C) 2006 Julien Rebetez
  *
@@ -21,6 +22,12 @@
 
 #include <config.h>
 #include "ev-form-field.h"
+#include "ev-form-field-private.h"
+
+typedef struct
+{
+	gchar   *alt_ui_name;
+} EvFormFieldPrivate;
 
 static void ev_form_field_init                 (EvFormField               *field);
 static void ev_form_field_class_init           (EvFormFieldClass          *klass);
@@ -33,31 +40,35 @@ static void ev_form_field_choice_class_init    (EvFormFieldChoiceClass    *klass
 static void ev_form_field_signature_init       (EvFormFieldSignature      *field_choice);
 static void ev_form_field_signature_class_init (EvFormFieldSignatureClass *klass);
 
-G_DEFINE_ABSTRACT_TYPE (EvFormField, ev_form_field, G_TYPE_OBJECT)
 G_DEFINE_TYPE (EvFormFieldText, ev_form_field_text, EV_TYPE_FORM_FIELD)
 G_DEFINE_TYPE (EvFormFieldButton, ev_form_field_button, EV_TYPE_FORM_FIELD)
 G_DEFINE_TYPE (EvFormFieldChoice, ev_form_field_choice, EV_TYPE_FORM_FIELD)
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (EvFormField, ev_form_field, G_TYPE_OBJECT)
 G_DEFINE_TYPE (EvFormFieldSignature, ev_form_field_signature, EV_TYPE_FORM_FIELD)
 
+#define GET_FIELD_PRIVATE(o) ev_form_field_get_instance_private (o)
 static void
 ev_form_field_init (EvFormField *field)
 {
+	EvFormFieldPrivate *priv = GET_FIELD_PRIVATE (field);
+
 	field->page = NULL;
 	field->changed = FALSE;
 	field->is_read_only = FALSE;
-	field->alt_ui_name = NULL;
+	priv->alt_ui_name = NULL;
 }
 
 static void
 ev_form_field_finalize (GObject *object)
 {
 	EvFormField *field = EV_FORM_FIELD (object);
+	EvFormFieldPrivate *priv = GET_FIELD_PRIVATE (field);
 
 	g_object_unref (field->page);
 	field->page = NULL;
 
 	g_clear_object (&field->activation_link);
-	g_clear_pointer (&field->alt_ui_name, g_free);
+	g_clear_pointer (&priv->alt_ui_name, g_free);
 
 	(* G_OBJECT_CLASS (ev_form_field_parent_class)->finalize) (object);
 }
@@ -68,6 +79,57 @@ ev_form_field_class_init (EvFormFieldClass *klass)
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
 	object_class->finalize = ev_form_field_finalize;
+}
+
+/**
+ * ev_form_field_get_alternate_name
+ * @field: a #EvFormField
+ *
+ * Gets the alternate ui name of @field. This name is also commonly
+ * used by pdf producers/readers to show it as a tooltip when @field area
+ * is hovered by a pointing device (eg. mouse).
+ *
+ * Returns: (transfer full): a string.
+ *
+ * Since: 3.38
+ **/
+gchar *
+ev_form_field_get_alternate_name (EvFormField *field)
+{
+	EvFormFieldPrivate *priv;
+
+	g_return_val_if_fail (EV_IS_FORM_FIELD (field), NULL);
+
+	priv = GET_FIELD_PRIVATE (field);
+
+	return priv->alt_ui_name;
+}
+
+/**
+ * ev_form_field_set_alternate_name
+ * @field: a #EvFormField
+ * @alternative_text: a string with the alternative name of a form field
+ *
+ * Sets the alternate ui name of @field. This name is also commonly
+ * used by pdf producers/readers to show it as a tooltip when @field area
+ * is hovered by a pointing device (eg. mouse).
+ *
+ * Since: 3.38
+ **/
+void
+ev_form_field_set_alternate_name (EvFormField *field,
+				  gchar       *alternative_text)
+{
+	EvFormFieldPrivate *priv;
+
+	g_return_if_fail (EV_IS_FORM_FIELD (field));
+
+	priv = GET_FIELD_PRIVATE (field);
+
+	if (priv->alt_ui_name)
+		g_clear_pointer (&priv->alt_ui_name, g_free);
+
+	priv->alt_ui_name = alternative_text;
 }
 
 static void

libdocument/ev-form-field.h
@@ -113,7 +113,6 @@ struct _EvFormField
 
 	EvPage  *page;
 	gboolean changed;
-	gchar   *alt_ui_name;
 };
 
 struct _EvFormFieldClass

libdocument/meson.build
@@ -27,6 +27,7 @@ headers = files(
   'ev-file-exporter.h',
   'ev-file-helpers.h',
   'ev-form-field.h',
+  'ev-form-field-private.h',
   'ev-image.h',
   'ev-init.h',
   'ev-layer.h',

libview/ev-view.c
@@ -35,6 +35,7 @@
 #include "ev-document-layers.h"
 #include "ev-document-media.h"
 #include "ev-document-misc.h"
+#include "ev-form-field-private.h"
 #include "ev-pixbuf-cache.h"
 #include "ev-page-cache.h"
 #include "ev-view-marshal.h"
@@ -2331,7 +2332,7 @@ ev_view_handle_cursor_over_xy (EvView *view, gint x, gint y)
 		}
 	}
 
-	if (link || annot || (field && field->alt_ui_name))
+	if (link || annot || (field && ev_form_field_get_alternate_name (field)))
 		g_object_set (view, "has-tooltip", TRUE, NULL);
 }
 
@@ -5320,14 +5321,18 @@ ev_view_query_tooltip (GtkWidget  *widget,
 	}
 
 	field = ev_view_get_form_field_at_location (view, x, y);
-	if (field && field->alt_ui_name && *(field->alt_ui_name) != '\0') {
-		GdkRectangle field_area;
+	if (field != NULL) {
+		gchar *alt_ui_name = ev_form_field_get_alternate_name (field);
 
-		get_field_area (view, x, y, field, &field_area);
-		gtk_tooltip_set_text (tooltip, field->alt_ui_name);
-		gtk_tooltip_set_tip_area (tooltip, &field_area);
+		if (alt_ui_name && *(alt_ui_name) != '\0') {
+			GdkRectangle field_area;
 
-		return TRUE;
+			get_field_area (view, x, y, field, &field_area);
+			gtk_tooltip_set_text (tooltip, alt_ui_name);
+			gtk_tooltip_set_tip_area (tooltip, &field_area);
+
+			return TRUE;
+		}
 	}
 
 	link = ev_view_get_link_at_location (view, x, y);