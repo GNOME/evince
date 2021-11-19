c1c18543361258dcc3d56879a28af1b12b579a43
Bastien Nocera <hadess@hadess.net>
2021-11-19 19:53:46 +0100

    unarr: Update LZMA SDK
    Using lzma2104.7z from:
    https://sourceforge.net/projects/sevenzip/files/LZMA%20SDK/

--- cut-n-paste/unarr/lzmasdk/CpuArch.c
+++ cut-n-paste/unarr/lzmasdk/CpuArch.c
@@ -72,7 +72,7 @@ static UInt32 CheckFlag(UInt32 flag)
  where ECX value is first parameter for FAST_CALL / NO_INLINE function,
  So the caller of MY__cpuidex_HACK() sets ECX as subFunction, and
  old MSVC for __cpuid() doesn't change ECX and cpuid instruction gets (subFunction) value.
-
+ 
  DON'T remove MY_NO_INLINE and MY_FAST_CALL for MY__cpuidex_HACK() !!!
 */
 
@@ -143,7 +143,7 @@ void MyCPUID(UInt32 function, UInt32 *a, UInt32 *b, UInt32 *c, UInt32 *d)
     : "0" (function), "c"(0) ) ;
 
   #endif
-
+  
   #else
 
   int CPUInfo[4];
@@ -197,7 +197,7 @@ BoolInt CPU_Is_InOrder(void)
 
   family = x86cpuid_GetFamily(p.ver);
   model = x86cpuid_GetModel(p.ver);
-
+  
   firm = x86cpuid_GetFirm(&p);
 
   switch (firm)
