d1dfee4fa980462c32ff9f9913c9475f2f9d7bef
Qiu Wenbo <qiuwenbo@kylinos.com.cn>
2022-08-16 12:44:36 +0800

    properties: Port to new Nautilus extension API
    Signed-off-by: Qiu Wenbo <qiuwenbo@kylinos.com.cn>

--- properties/ev-properties-view.c
+++ properties/ev-properties-view.c
@@ -33,49 +33,6 @@
 
 #include "ev-properties-view.h"
 
-typedef enum {
-	TITLE_PROPERTY,
-	URI_PROPERTY,
-	SUBJECT_PROPERTY,
-	AUTHOR_PROPERTY,
-	KEYWORDS_PROPERTY,
-	PRODUCER_PROPERTY,
-	CREATOR_PROPERTY,
-	CREATION_DATE_PROPERTY,
-	MOD_DATE_PROPERTY,
-	N_PAGES_PROPERTY,
-	LINEARIZED_PROPERTY,
-	FORMAT_PROPERTY,
-	SECURITY_PROPERTY,
-	CONTAINS_JS_PROPERTY,
-	PAPER_SIZE_PROPERTY,
-	FILE_SIZE_PROPERTY,
-	N_PROPERTIES,
-} Property;
-
-typedef struct {
-	Property property;
-	const char *label;
-} PropertyInfo;
-
-static const PropertyInfo properties_info[] = {
-	{ TITLE_PROPERTY,         N_("Title:") },
-	{ URI_PROPERTY,           N_("Location:") },
-	{ SUBJECT_PROPERTY,       N_("Subject:") },
-	{ AUTHOR_PROPERTY,        N_("Author:") },
-	{ KEYWORDS_PROPERTY,      N_("Keywords:") },
-	{ PRODUCER_PROPERTY,      N_("Producer:") },
-	{ CREATOR_PROPERTY,       N_("Creator:") },
-	{ CREATION_DATE_PROPERTY, N_("Created:") },
-	{ MOD_DATE_PROPERTY,      N_("Modified:") },
-	{ N_PAGES_PROPERTY,       N_("Number of Pages:") },
-	{ LINEARIZED_PROPERTY,    N_("Optimized:") },
-	{ FORMAT_PROPERTY,        N_("Format:") },
-	{ SECURITY_PROPERTY,      N_("Security:") },
-	{ CONTAINS_JS_PROPERTY,   N_("Contains Javascript:") },
-	{ PAPER_SIZE_PROPERTY,    N_("Paper Size:") },
-	{ FILE_SIZE_PROPERTY,     N_("Size:") }
-};
 
 struct _EvPropertiesView {
 	GtkBox base_instance;
@@ -297,7 +254,7 @@ get_tolerance (gdouble size)
 		return 3.0f;
 }
 
-static char *
+char *
 ev_regular_paper_size (const EvDocumentInfo *info)
 {
 	GList *paper_sizes, *l;
