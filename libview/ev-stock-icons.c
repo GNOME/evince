ab38744eaec62934aa29b9cba0254401491a24a5
Pablo Correa Gómez <ablocorrea@hotmail.com>
2023-03-07 17:18:55 +0100

    ev-stock-icons: remove GtkIconFactory code
    This code comes from e90ec741051727680a653837fb7b401192c507f7, copied
    directly from gpdf. The purpose of this code seems to have been
    to provide aliases to icons (so they could be called from 2 different names),
    and serve a similar purpose as GtkIconTheme does today.
    
    The whole concept of GtkIconFactory, GtkIconSet, and GtkIconSource was
    deprecated in GTK 3.10 back in 2013, so simply get rid of it. This has
    no impact in Evince, since the icons can still be found through GtkIconTheme.
    
    To make sure that this is a non-breaking change in libevview, rename
    stock_filters-invert icon. The said icon is exported in the API through
    
    and the internal name "stock_filters-invert" was never documented. Therefore,
    renaming the icon to "inverted" as exposed by the API should still make the
    icon available to consumers (via EV_STOCK_INVERTED_COLORS), without the need
    to use GtkIconFactory.

--- libview/ev-stock-icons.c
+++ libview/ev-stock-icons.c
@@ -25,35 +25,6 @@
 
 #include "ev-stock-icons.h"
 
-typedef struct {
-	char *stock_id;
-	char *icon;
-} EvStockIcon;
-
-/* Evince stock icons */
-static const EvStockIcon stock_icons [] = {
-	{ EV_STOCK_ANNOT_TEXT,       "annotation-text-symbolic" },
-	{ EV_STOCK_ANNOT_SQUIGGLY,   "annotation-squiggly-symbolic" },
-	{ EV_STOCK_FIND_UNSUPPORTED, "find-unsupported-symbolic" },
-	{ EV_STOCK_ZOOM,	     "zoom" },
-	{ EV_STOCK_ZOOM_PAGE,        "zoom-fit-height" },
-	{ EV_STOCK_ZOOM_WIDTH,       "zoom-fit-width" },
-	{ EV_STOCK_VIEW_DUAL,        "view-page-facing" },
-	{ EV_STOCK_VIEW_CONTINUOUS,  "view-page-continuous" },
-	{ EV_STOCK_ROTATE_LEFT,      "object-rotate-left"},
-	{ EV_STOCK_ROTATE_RIGHT,     "object-rotate-right"},
-	{ EV_STOCK_RUN_PRESENTATION, "x-office-presentation"},
-	{ EV_STOCK_VISIBLE,          "visible-symbolic"},
-	{ EV_STOCK_RESIZE_SE,        "resize-se"},
-	{ EV_STOCK_RESIZE_SW,        "resize-sw"},
-	{ EV_STOCK_CLOSE,            "close"},
-	{ EV_STOCK_INVERTED_COLORS,  "stock_filters-invert"},
-	{ EV_STOCK_ATTACHMENT,       "mail-attachment"},
-	{ EV_STOCK_SEND_TO,          "document-send"},
-	{ EV_STOCK_VIEW_SIDEBAR,     "view-sidebar-symbolic"},
-	{ EV_STOCK_OUTLINE,          "outline-symbolic"},
-};
-
 static gchar *ev_icons_path;
 
 static void
@@ -94,9 +65,6 @@ ev_stock_icons_add_icons_path_for_screen (GdkScreen *screen)
 void
 ev_stock_icons_init (void)
 {
-	GtkIconFactory *factory;
-	GtkIconSource *source;
-	gint i;
 #ifdef G_OS_WIN32
 	gchar *dir;
 
@@ -108,27 +76,6 @@ ev_stock_icons_init (void)
 	if (g_getenv ("EV_ICONS_DIR") != NULL)
 		ev_icons_path = g_build_filename (g_getenv ("EV_ICONS_DIR"), NULL);
 #endif
-
-        factory = gtk_icon_factory_new ();
-        gtk_icon_factory_add_default (factory);
-
-	source = gtk_icon_source_new ();
-
-	for (i = 0; i < G_N_ELEMENTS (stock_icons); i++) {
-		GtkIconSet *set;
-
-		gtk_icon_source_set_icon_name (source, stock_icons [i].icon);
-
-		set = gtk_icon_set_new ();
-		gtk_icon_set_add_source (set, source);
-
-		gtk_icon_factory_add (factory, stock_icons [i].stock_id, set);
-		gtk_icon_set_unref (set);
-	}
-
-	gtk_icon_source_free (source);
-
-	g_object_unref (G_OBJECT (factory));
 
 	ev_stock_icons_add_icons_path_for_screen (gdk_screen_get_default ());
 }
