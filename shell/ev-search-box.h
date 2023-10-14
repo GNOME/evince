adbe70f37614645ddd555f78a7cb871b8b487b5d
Pablo Correa Gomez <ablocorrea@hotmail.com>
2023-10-14 19:27:25 +0000

    libmisc: move ev-search-box from libmisc to shell

--- shell/ev-search-box.h
+++ shell/ev-search-box.h
@@ -26,25 +26,13 @@
 
 G_BEGIN_DECLS
 
-#define EV_TYPE_SEARCH_BOX            (ev_search_box_get_type ())
-#define EV_SEARCH_BOX(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), EV_TYPE_SEARCH_BOX, EvSearchBox))
-#define EV_IS_SEARCH_BOX(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), EV_TYPE_SEARCH_BOX))
-#define EV_SEARCH_BOX_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), EV_TYPE_SEARCH_BOX, EvSearchBoxClass))
-#define EV_IS_SEARCH_BOX_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((obj), EV_TYPE_SEARCH_BOX))
-#define EV_SEARCH_BOX_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS((obj), EV_TYPE_SEARCH_BOX, EvSearchBoxClass))
+#define EV_TYPE_SEARCH_BOX (ev_search_box_get_type())
 
-typedef struct _EvSearchBox        EvSearchBox;
-typedef struct _EvSearchBoxClass   EvSearchBoxClass;
+G_DECLARE_FINAL_TYPE (EvSearchBox, ev_search_box, EV, SEARCH_BOX, GtkBox);
 
 struct _EvSearchBox {
-        GtkBox parent;
+	GtkBox parent;
 };
-
-struct _EvSearchBoxClass {
-        GtkBoxClass parent_class;
-};
-
-GType           ev_search_box_get_type    (void);
 
 GtkWidget      *ev_search_box_new         (EvDocumentModel *model);
 GtkSearchEntry *ev_search_box_get_entry   (EvSearchBox     *box);
