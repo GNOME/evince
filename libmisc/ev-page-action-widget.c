GNOME/evince

commit 7c28e563924c4ffad33e31f209bb75ab6c9c8eb9

libmisc: port to g_clear_signal_handler
Relates #1988

libmisc/ev-page-action-widget.c
@@ -290,13 +290,8 @@ ev_page_action_widget_set_document (EvPageActionWidget *action_widget,
                 gtk_widget_set_sensitive (GTK_WIDGET (action_widget), ev_document_get_n_pages (document) > 0);
         }
 
-        if (action_widget->signal_id > 0) {
-                if (action_widget->doc_model != NULL) {
-                        g_signal_handler_disconnect (action_widget->doc_model,
-                                                     action_widget->signal_id);
-                }
-                action_widget->signal_id = 0;
-        }
+	g_clear_signal_handler (&action_widget->signal_id,
+				action_widget->doc_model);
 
         if (action_widget->document)
                 g_object_unref (action_widget->document);
@@ -348,17 +343,11 @@ ev_page_action_widget_finalize (GObject *object)
 {
 	EvPageActionWidget *action_widget = EV_PAGE_ACTION_WIDGET (object);
 
+	g_clear_signal_handler (&action_widget->signal_id,
+				action_widget->doc_model);
+	g_clear_signal_handler (&action_widget->notify_document_signal_id,
+				action_widget->doc_model);
 	if (action_widget->doc_model != NULL) {
-		if (action_widget->signal_id > 0) {
-			g_signal_handler_disconnect (action_widget->doc_model,
-						     action_widget->signal_id);
-			action_widget->signal_id = 0;
-		}
-		if (action_widget->notify_document_signal_id > 0) {
-			g_signal_handler_disconnect (action_widget->doc_model,
-						     action_widget->notify_document_signal_id);
-			action_widget->notify_document_signal_id = 0;
-		}
 		g_object_remove_weak_pointer (G_OBJECT (action_widget->doc_model),
 					      (gpointer)&action_widget->doc_model);
 		action_widget->doc_model = NULL;

commit 6dd371e5adac26871764cf7f1302013d8d8ce65a

libmisc: Fix regression after porting to g_clear_signal_handler
Fix regression introduced in 7c28e56392, in which there
should only clear when doc_model has a value (because
doc_model could be NULL, and handlers might not, and still
be a valid situation).

The error can be reproduced whenever an action is taken, e.g.
in evince-previewer, by clicking to zoom in or zoom out.

libmisc/ev-page-action-widget.c
@@ -300,8 +300,9 @@ ev_page_action_widget_set_document (EvPageActionWidget *action_widget,
                 gtk_widget_set_sensitive (GTK_WIDGET (action_widget), ev_document_get_n_pages (document) > 0);
         }
 
-	g_clear_signal_handler (&action_widget->signal_id,
-				action_widget->doc_model);
+        if (action_widget->doc_model != NULL)
+                g_clear_signal_handler (&action_widget->signal_id,
+                                        action_widget->doc_model);
 
         if (action_widget->document)
                 g_object_unref (action_widget->document);
@@ -365,11 +366,11 @@ ev_page_action_widget_finalize (GObject *object)
 {
 	EvPageActionWidget *action_widget = EV_PAGE_ACTION_WIDGET (object);
 
-	g_clear_signal_handler (&action_widget->signal_id,
-				action_widget->doc_model);
-	g_clear_signal_handler (&action_widget->notify_document_signal_id,
-				action_widget->doc_model);
 	if (action_widget->doc_model != NULL) {
+		g_clear_signal_handler (&action_widget->signal_id,
+					action_widget->doc_model);
+		g_clear_signal_handler (&action_widget->notify_document_signal_id,
+					action_widget->doc_model);
 		g_object_remove_weak_pointer (G_OBJECT (action_widget->doc_model),
 					      (gpointer)&action_widget->doc_model);
 		action_widget->doc_model = NULL;